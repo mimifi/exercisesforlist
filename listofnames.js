da09b91e9fe8c002a16f1aa49830459db486d008
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-11-23 21:33:28 +1100

    feat: create a list of names in html

---
+++ listofnames.js
@@ -0,0 +1,13 @@
+const myNames = ["Mahtab", "Max", "Marjan", "Isaac"]
+
+function listOfRequiredNames(arrayNames) {
+    const ul = document.createElement("ul");
+
+    for (let i = 0; i < arrayNames.length; i++) {
+        const listItem = document.createElement("li");
+        const textOfItem = document.createTextNode(arrayNames[i]);
+        listItem.appendChild(textOfItem);
+        ul.appendChild(listItem);
+    }
+    document.body.appendChild(ul);
+}
